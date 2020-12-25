0dc813edd3ed994da5aaa4808cf604dcc2aa14b7
edfadeev <dr.eduard.fadeev@gmail.com>
2020-12-25 09:14:17 +0100

    comparison to PS107

--- MESO_comparison_to_PS107.R
+++ MESO_comparison_to_PS107.R
@@ -43,62 +43,20 @@ PS107_SRF_ps0 <- subset_taxa(PS107_SRF_ps, !Kingdom %in% c("Eukaryota") & !Phylu
 
 
 #####################################
-#Bar plots of PS107 surface samples
+#Merge datasets on genus level
 #####################################
-#transform data
-BAC_pruned.ra <- transform_sample_counts(PS107_SRF_ps0, function(x) x / sum(x))
-BAC_pruned.ra.long <- psmelt(BAC_pruned.ra)
-
-#calculate abundance for each Class and replace classes below 1% with "Other taxa"
-BAC_pruned.ra.long.agg <- BAC_pruned.ra.long %>% select(StationName,Class,Abundance)%>%
-  group_by(StationName,Class) %>%
-  summarize(Abund.total= sum(Abundance)*100)  %>% 
-  filter(Abund.total>0)
-
-taxa_classes <- unique(BAC_pruned.ra.long.agg$Class)
-
-BAC_pruned.ra.long.agg<- BAC_pruned.ra.long.agg %>% 
-  mutate(Class = ifelse(Abund.total < 1, "Other taxa < 2%", Class))%>% 
-  mutate(Class =factor(Class, levels = c(taxa_classes,"Other taxa < 2%")))
-
-#Plot 
-barplots <- ggplot(BAC_pruned.ra.long.agg, aes(x = StationName, y = Abund.total, fill = Class)) + 
-  #facet_grid(Type~Community, space= "fixed") +
-  geom_col()+
-  scale_fill_manual(values = tol21rainbow) + 
-  guides(fill = guide_legend(reverse = FALSE, keywidth = 1, keyheight = 1)) +
-  ylab("Sequence proportions (%) \n")+
-  theme_classic()+
-  theme(legend.position = "bottom")
-
-
-
-
-meso_ps0.up50m <- subset_samples(meso_ps0, Type %in% c("Surface-10", "Chl.max-20-30")) #& Community == "FL")
+meso_ps0.prev <-  readRDS("./Data/MESO_ps0_prev.rds")
+meso_ps0.up50m <- subset_samples(meso_ps0.prev, Type %in% c("Surface-10"))
 sample_data(meso_ps0.up50m)$merging <- paste(sample_data(meso_ps0.up50m)$StationName,sample_data(meso_ps0.up50m)$Depth, sep ="-")
 meso_ps0.up50m.merged<- merge_samples(meso_ps0.up50m, "merging",  fun=sum)
-sample_names(meso_ps0.up50m.merged)<- paste(sample_data(meso_ps0.up50m.merged)$StationName,sample_data(meso_ps0.up50m.merged)$Depth,sep ="-")
+meso_ps0.up50m.merged.glom<- tax_glom(meso_ps0.up50m.merged, "Genus")
+
 
 sample_names(PS107_SRF_ps0)<- sample_data(PS107_SRF_ps0)$StationName
-
-
-merged_ps<- merge_phyloseq(meso_ps0.up50m.merged,PS107_SRF_ps0)
-sample_data(merged_ps)$StationName<- sample_names(merged_ps)
-
-sample_names(meso_ps0.up50m)<- paste(sample_data(meso_ps0.up50m)$StationName,sample_data(meso_ps0.up50m)$Community,sample_data(meso_ps0.up50m)$Depth,sep ="-")
-
-
-
-
-meso_ps0.up50m.merged.glom<- tax_glom(meso_ps0.up50m, "Genus")
-
-
-merged_ps<- merge_phyloseq(meso_ps0.up50m,PS107_SRF_ps0)
-
 PS107_SRF_ps0.glom<- tax_glom(PS107_SRF_ps0, "Genus")
 
 merged_ps<- merge_phyloseq(meso_ps0.up50m.merged.glom,PS107_SRF_ps0.glom)
-
+sample_data(merged_ps)$StationName<- sample_names(merged_ps)
 
 #transform data
 BAC_pruned.ra <- transform_sample_counts(merged_ps, function(x) x / sum(x))
@@ -123,49 +81,62 @@ BAC_pruned.ra.long.agg<- BAC_pruned.ra.long.agg %>%
 barplots <- ggplot(BAC_pruned.ra.long.agg, aes(x = StationName, y = Abund.total, fill = Class)) + 
   #facet_grid(Type~Community, space= "fixed", scales = "free") +
   geom_col()+
-  scale_fill_manual(values = tol21rainbow) + 
+  scale_fill_manual(values = class_col) + 
   guides(fill = guide_legend(reverse = FALSE, keywidth = 1, keyheight = 1)) +
   ylab("Sequence proportions (%) \n")+
-  theme_classic()+
-  theme(legend.position = "bottom")
+  theme_bw()+
+  theme(panel.grid.major = element_blank(),panel.grid.minor = element_blank(), 
+        axis.line = element_line(colour = "black"),#axis.text.x = element_blank(),
+        text=element_text(size=14),legend.position = "bottom")
+
+ggsave("./figures/PS107_meso.pdf", barplots, dpi = 300, 
+       #width = 30, height = 30, 
+       units = "cm")
 
 
 
 #####################################
 #NMDS plot of upper 50 m
 #####################################
-#subset the surface samples
-merged_ps.gm<- phyloseq_gm_mean_trans(merged_ps)
-
-meso_ps0.gm_mean_up50m <- subset_samples(meso_ps0.gm_mean, Type %in% c("Surface-10", 
-                                                                       "Chl.max-20-30",
-                                                                       "B.Chl.max-50"))
-
 BAC_pruned.ra <- transform_sample_counts(merged_ps, function(x) x / sum(x))
 
 #NMDS plot
-merged_ps.gm.ord <- ordinate(merged_ps.gm, method = "NMDS", distance = "euclidean")
-merged_ps.gm.df <- plot_ordination(merged_ps.gm, merged_ps.gm.ord, axes = c(1,2,3),justDF = TRUE)
+merged_ps.gm.ord <- ordinate(BAC_pruned.ra, method = "NMDS", distance = "bray")
+merged_ps.gm.df <- plot_ordination(merged_ps, merged_ps.gm.ord, axes = c(1,2,3),justDF = TRUE)
 
 PS107.NMDS_up50m.p <- ggplot(data = merged_ps.gm.df, aes(x = NMDS1, y = NMDS2, label  = StationName))+
   geom_point(colour = "black", size = 5) +
   geom_point(size = 4) +
-  geom_text(nudge_y= -5,size=3, colour = "black")+
+  geom_text(nudge_y= -0.02,size=3, colour = "black")+
   scale_colour_manual(values = c("in"="red1",
                                  "out"="blue1")) + 
-  #annotate(geom="text", x=-70, y=50, label= paste0("Stress = ", round(merged_ps.gm.ord$stress,2)),
-  #         color="black", size = 5)+
-  theme_classic()+
-  theme(legend.position = "bottom")
+  annotate(geom="text", x=-0.6, y=0.7, label= paste0("Stress = ", round(merged_ps.gm.ord$stress,2)),
+           color="black", size = 5)+
+  geom_hline(yintercept = 0.15, linetype = 2, alpha = 0.5)+
+  coord_fixed()+
+  theme_bw()+
+  theme(panel.grid.major = element_blank(),panel.grid.minor = element_blank(), 
+        axis.line = element_line(colour = "black"),#axis.text.x = element_blank(),
+        text=element_text(size=14),legend.position = "bottom")
 
-ggsave("./figures/NMDS_SP107_surf.pdf", meso_ps0.NMDS_up50m.p, dpi = 300, 
+ggsave("./figures/NMDS_SP107_surf.pdf", PS107.NMDS_up50m.p, dpi = 300, 
        #width = 11.4, height = 23, 
        units = "cm")
 
 
+#significance test
+df <- as(sample_data(BAC_pruned.ra), "data.frame") %>% 
+          mutate(Bloom = case_when(StationName %in% c("EG1","EG4","T1-10","T1-20","T2-10","T2-30","T5-10","T5-25") ~ "YES",
+                                                      TRUE~"NO"),
+                 Dataset = case_when(StationName %in% c("T1-10","T1-20","T2-10","T2-30","T5-10","T5-25","T3-10","T3-30","T4-10","T4-30") ~ "sub",
+                 TRUE~"PS107"))
 
 
+d <- phyloseq::distance(BAC_pruned.ra, "bray")
+adonis_all <- adonis2(d ~ Bloom + Dataset, df)
+adonis_all
 
+ 
 
 #####################################
 #get session info and remove all objects and libraries
